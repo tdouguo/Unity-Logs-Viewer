d7c68f954ebc583d17dd782c22ce83b8a026920b
GuoJinMing@mbp <tdouguo@163.com>
2021-03-09 20:23:37 +0800

    shell

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
-cat ./package.json | jq 'del(.version)' | jq '. + { "version": "sdkver" }'>>tmp
-mv tmp ./package.json
-sed -i '' 's/sdkver/'"$1"'/'  ./package.json
+cat Unity-Logs-Viewer/package.json | jq 'del(.version)' | jq '. + { "version": "sdkver" }'>>tmp
+mv tmp Unity-Logs-Viewer/package.json
+sed -i '' 's/sdkver/'"$1"'/'  Unity-Logs-Viewer/package.json
 git add -A
 git commit -m "uv"
 git pull
 git push
-git subtree split --prefix=. --branch upm
+git subtree split --prefix=Unity-Logs-Viewer --branch upm
 git tag $1 upm
 git push origin upm --tags
